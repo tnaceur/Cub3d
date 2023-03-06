5346c9d70ff3b639fb16fc21d361d2c5cff4abe8
Taha Naceur <tnaceur@e1r1p8.1337.ma>
2023-03-06 14:43:11 +0100

    Error and somme Costimasition

--- draw_utils2.c
+++ draw_utils2.c
@@ -6,7 +6,7 @@
 /*   By: tnaceur <tnaceur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 09:43:56 by tnaceur           #+#    #+#             */
-/*   Updated: 2023/03/06 09:46:36 by tnaceur          ###   ########.fr       */
+/*   Updated: 2023/03/06 14:41:56 by tnaceur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,6 @@ int	player_pos(t_game *game, double x2, double y2)
 	y = floor(y2 / 40);
 	if ((x >= 0 && y >= 0 && game->map[x] && game->map[x][y] == '1')
 		|| x >= game->hieght || y >= (int)ft_strlen(game->map[x]))
-		return (0);
-	x = floor((x2 + 1) / 40);
-	y = floor(y2 / 40);
-	if (x >= 0 && y >= 0 && game->map[x] && game->map[x][y] == '1')
-		return (0);
-	x = floor(x2 / 40);
-	y = floor((y2 + 1) / 40);
-	if (x >= 0 && y >= 0 && game->map[x] && game->map[x][y] == '1')
-		return (0);
-	x = floor((x2 - 1) / 40);
-	y = floor(y2 / 40);
-	if (x >= 0 && y >= 0 && game->map[x] && game->map[x][y] == '1')
-		return (0);
-	x = floor(x2 / 40);
-	y = floor((y2 - 1) / 40);
-	if (x >= 0 && y >= 0 && game->map[x] && game->map[x][y] == '1')
 		return (0);
 	return (1);
 }
